1699f539e0b64164ab43ec7cfca885c2e39a9892
muhammadsajid3674 <sajiddark3674@gmail.com>
2023-03-31 20:13:00 +0500

    shift callback from route to controller

--- controller/task.controller.js
+++ controller/task.controller.js
@@ -1,28 +1,32 @@
 const logger = require("../logger/api.logger");
-const taskService = require("../service/task.service");
+const taskRepository = require("../repository/task.repository");
 
-class TodoController {
-
-    async getTask() {
-        logger.info('Controller: getTask');
-        return await taskService.getTask();
-    }
-
-    async createTask(task) {
-        logger.info('Controller: createTask', task);
-        return await taskService.createTask(task);
-    }
-
-    async updateTask(query, task) {
-        logger.info('Controller: updateTask', { query, task });
-        return await taskService.updateTask(query, task);
-    }
-
-    async deleteTask(taskId) {
-        logger.info('Controller: deleteTask', taskId);
-        return await taskService.deleteTask(taskId);
-    }
-
+const getTask = (req, res) => {
+    logger.info('Controller: getTask');
+    taskRepository.getTask().then(data => res.json(data));
 }
 
-module.exports = new TodoController();
+const createTask = (req, res) => {
+    const { task } = req.body;
+    console.log(task);
+    task.createAt = Date.now();
+    taskRepository.createTask(req.body.task).then(data => res.json(data))
+}
+
+const updateTask = (req, res) => {
+    const { task } = req.body;
+    const { id } = req.params;
+    taskRepository.updateTask({ _id: id }, task).then((data) => res.json(data))
+}
+
+const deleteTask = (req, res) => {
+    const { id } = req.params;
+    taskRepository.deleteTask(id).then(data => res.json(data))
+}
+
+module.exports = {
+    getTask,
+    createTask,
+    updateTask,
+    deleteTask
+}
--- routes/task.routes.js
+++ routes/task.routes.js
@@ -1,28 +1,8 @@
 const express = require("express");
-const taskController = require("../controller/task.controller");
-
+const { getTask, createTask, updateTask, deleteTask } = require("../controller/task.controller");
 const taskRouter = express.Router();
 
-taskRouter.get('/', (req, res) => {
-    taskController.getTask().then(data => res.json(data));
-})
-
-taskRouter.post('/', (req, res) => {
-    const { task } = req.body;
-    console.log(task);
-    task.createAt = Date.now();
-    taskController.createTask(req.body.task).then(data => res.json(data))
-})
-
-taskRouter.put('/:id', (req, res) => {
-    const { task } = req.body;
-    const { id } = req.params;
-    taskController.updateTask({ _id: id }, task).then((data) => res.json(data))
-})
-
-taskRouter.delete('/:id', (req, res) => {
-    const { id } = req.params;
-    taskController.deleteTask(id).then(data => res.json(data))
-})
+taskRouter.route('/').get(getTask).post(createTask)
+taskRouter.route('/:id').put(updateTask).delete(deleteTask)
 
 module.exports = taskRouter;
